7aeca40c855b18d0b8050e6b6af7dc8cf9abec8a
Christopher Zielinski <ckz8780@gmail.com>
2018-07-22 19:03:35 -0400

    added tests for user updates

--- users/tests.py
+++ users/tests.py
@@ -6,22 +6,32 @@ from django.contrib.auth.models import User
 class UserTests(APITestCase):
 
     def setUp(self):
+        """
+        Setup stuff: create three users to play with, two regular and one admin.
+        """
         self.test_admin_user = User.objects.create_superuser('testadminuser', 'test@example.com', 'testadminpassword')
         self.test_user = User.objects.create_user('testuser', 'test@example.com', 'testpassword')
+        self.test_user2 = User.objects.create_user('testuser2', 'test@example.com', 'testpassword2')
 
     def test_setup_created_correct_test_users(self):
         """
         Confirm that we have both a regular user and an admin user to test with.
         """
         test_user = User.objects.get(username="testuser")
+        test_user2 = User.objects.get(username="testuser2")
         test_admin_user = User.objects.get(username="testadminuser")
 
         self.assertIsInstance(test_user, User)
+        self.assertIsInstance(test_user2, User)
         self.assertIsInstance(test_admin_user, User)
 
         self.assertTrue(test_user.is_active)
         self.assertFalse(test_user.is_staff)
         self.assertFalse(test_user.is_superuser)
+
+        self.assertTrue(test_user2.is_active)
+        self.assertFalse(test_user2.is_staff)
+        self.assertFalse(test_user2.is_superuser)
         
         self.assertTrue(test_admin_user.is_active)
         self.assertTrue(test_admin_user.is_staff)
@@ -62,12 +72,11 @@ class UserTests(APITestCase):
         }
         response = self.client.post(url, data, format='json')
         ignore_response_fields = ['id', 'password']
-
         for k, v in response.data.items():
             if k not in ignore_response_fields:
                 self.assertEqual(v, data[k])
 
-    def test_password_not_in_api_response(self):
+    def test_password_not_in_create_user_api_response(self):
         """
         Confirm that the API does not return the password in the response.
         """
@@ -82,7 +91,6 @@ class UserTests(APITestCase):
         response = self.client.post(url, data, format='json')
 
         self.assertNotIn('password', response.data.keys())
-
 
     def test_cannot_update_user_anonymously(self):
         """
@@ -103,3 +111,75 @@ class UserTests(APITestCase):
         response_status = response.status_code
 
         self.assertEqual(response_status, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response_detail, 'Authentication credentials were not provided.')
+
+    def test_regular_user_cannot_update_other_users_info(self):
+        """
+        Confirm that one unprivileged (non-staff/su) user cannot update another's info
+        """
+        url = reverse('update-user', args=[2]) # Try to update the test user (pk=2)
+        data = {
+            "username": "updated_test_user",
+            "first_name": "Updated",
+            "last_name": "User",
+            "email": "updated@example.com",
+            "password": "totally_different_password!"
+        }
+        
+        self.client.login(username='testuser2', password='testpassword2')
+        response = self.client.put(url, data, format='json')
+        response_detail = response.data['detail']
+        response_status = response.status_code
+
+        self.assertEqual(response_status, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response_detail, 'You do not have permission to perform this action.')
+
+    def test_user_can_update_own_info(self):
+        """
+        Confirm that a user can update their own user info
+        """
+        url = reverse('update-user', args=[2]) # Try to update the test user (pk=2)
+        data = {
+            "username": "updated_test_user",
+            "first_name": "Updated",
+            "last_name": "User",
+            "email": "updated@example.com",
+            "password": "totally_different_password!"
+        }
+        
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.put(url, data, format='json')
+        response_status = response.status_code
+        user = User.objects.get(username='updated_test_user')
+        ignore_response_fields = ['id', 'password']
+
+        self.assertEqual(response_status, status.HTTP_200_OK)
+        self.assertIsInstance(user, User)
+        for k, v in response.data.items():
+            if k not in ignore_response_fields:
+                self.assertEqual(v, getattr(user, k))
+
+    def test_admin_user_can_update_regular_users_info(self):
+        """
+        Confirm that an admin user can update someone else's user info
+        """
+        url = reverse('update-user', args=[2]) # Try to update the test user (pk=2)
+        data = {
+            "username": "updated_test_user",
+            "first_name": "Updated",
+            "last_name": "User",
+            "email": "updated@example.com",
+            "password": "totally_different_password!"
+        }
+        
+        self.client.login(username='testadminuser', password='testadminpassword')
+        response = self.client.put(url, data, format='json')
+        response_status = response.status_code
+        user = User.objects.get(username='updated_test_user')
+        ignore_response_fields = ['id', 'password']
+
+        self.assertEqual(response_status, status.HTTP_200_OK)
+        self.assertIsInstance(user, User)
+        for k, v in response.data.items():
+            if k not in ignore_response_fields:
+                self.assertEqual(v, getattr(user, k))
