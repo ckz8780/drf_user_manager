e11f1106d1331ed35682a3642f8aaf4ec225874c
Christopher Zielinski <ckz8780@gmail.com>
2018-07-21 20:32:15 -0400

    Added password/email validation to UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,42 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 
+import django.contrib.auth.password_validation as django_pw_validators
+
 class UserSerializer(serializers.ModelSerializer):
+
+    # Validate that emails are unique as well as usernames
+    email = serializers.EmailField(
+            required=True,
+            validators=[UniqueValidator(queryset=User.objects.all())]
+            )
+
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email',)
-        write_only_fields = ('password',)
+        fields = ('username', 'first_name', 'last_name', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def validate(self, data):
+        """
+        Validate passwords by utilizing Django's built in validators
+        """
+        user = User(**data)
+        password = data.get('password')
+        errors = dict() 
+        
+        try:
+            django_pw_validators.validate_password(password=password, user=user)
+        except ValidationError as e:
+            errors['password'] = list(e.messages)
+
+        if errors:
+            raise serializers.ValidationError(errors)
+
+        return super(UserSerializer, self).validate(data)
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.save()
+        return user
