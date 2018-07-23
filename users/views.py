dadebdd14f44204b9f47e550a4d6d6fbd0561fbd
Christopher Zielinski <ckz8780@gmail.com>
2018-07-23 01:20:32 -0400

    Removed erroneous render import from users.views

--- users/views.py
+++ users/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
