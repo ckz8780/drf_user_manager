7fba197453e450957e31dedce5c9307dcbf89201
Christopher Zielinski <ckz8780@gmail.com>
2018-07-23 00:46:28 -0400

    added simple frontend for usage examples

--- drf_user_manager/urls.py
+++ drf_user_manager/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', TemplateView.as_view(template_name="index.html")),
     url(r'^api/v1/users/', include('users.urls')),
 ]
