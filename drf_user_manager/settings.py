61063186506f1056e2f764a332008f33ff194858
Christopher Zielinski <ckz8780@gmail.com>
2018-07-22 22:37:19 -0400

    Added whitenoise for staticfiles in prod

--- drf_user_manager/settings.py
+++ drf_user_manager/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'drf_user_manager.urls'
@@ -138,3 +139,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
